ffebcfeb094e538dd0a7c5726ce5fa8aa9803e80
Drue Johnson <drueby@gmail.com>
2018-08-01 20:30:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
 
+  configure do
+  	set :views, "app/views"
+  	set :public_dir, "public"
+  end
 
+  get '/' do
+  	erb :index
+  end
+  
+  post '/' do
+    @my_results = Questions(params[:question1, :question2, :question3, :question4, :question5, :question6, :question7, :question8])
+      
+      erb :results
+  end
+  
+  # get "/results" do
+  # 	erb :results
+  # end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,20 @@
-require_relative '../config/environment.rb'
-require 'rack/test'
-RACK_ENV = "test"
+ENV["SINATRA_ENV"] = "test"
 ENV["RACK_ENV"] = "test"
 
+require_relative '../config/environment'
+
+require 'rack/test'
+
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.include Rack::Test::Methods
 
   config.order = 'default'
 end
+
+def app
+  Rack::Builder.parse_file('config.ru').first
+end
+
 
 def app
   Rack::Builder.parse_file('config.ru').first
